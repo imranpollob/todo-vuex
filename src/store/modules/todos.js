64ea4f3179e6e0d5d73672f432008863cb172077
Imran <polboy777@gmail.com>
2019-09-27 12:49:50 +0600

    fetch todos action

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,25 +1,28 @@
 import axios from "axios";
 
 const state = {
-  todos: [
-    {
-      id: 1,
-      title: "first todo"
-    },
-    {
-      id: 2,
-      title: "second todo"
-    }
-  ]
+  todos: []
 };
 
 const getters = {
   allTodos: state => state.todos
 };
 
-const actions = {};
+const actions = {
+  async fetchTodos({ commit }) {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos"
+    );
 
-const mutations = {};
+    commit("setTodos", response.data);
+  }
+};
+
+const mutations = {
+  setTodos(state, todos) {
+    state.todos = todos;
+  }
+};
 
 export default {
   state,
